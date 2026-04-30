9bf5921df668f5d76c44b0af8dfe5d5996693503
keerifox <git@keeri.place>
2026-04-30 20:31:39 +0000

    missing person scenario, more dialogue lines, 10% less gender

--- Modules/JunkieRehabilitationModule/Module.gd
+++ Modules/JunkieRehabilitationModule/Module.gd
@@ -2,7 +2,13 @@ extends Module
 class_name JunkieRehabilitationModule
 
 func getFlags():
-	return {}
+	return {
+		"Scenario_MissingPerson_ChanceMillionth": flag(FlagType.Number),
+		"Scenario_MissingPerson_Name": flag(FlagType.Text),
+		"Scenario_MissingPerson_PronounThem": flag(FlagType.Text),
+		"Scenario_MissingPerson_SeekerName": flag(FlagType.Text),
+		"Scenario_MissingPerson_SearchAttempts": flag(FlagType.Number),
+	}
 
 func _init():
 	id = "JunkieRehabilitationModule"
@@ -26,3 +32,35 @@ func postInit():
 	GlobalRegistry.registerScene("res://Modules/JunkieRehabilitationModule/Scenes/Overwrites/DrugDenEncounterInstantFightScene.gd")
 	GlobalRegistry.registerScene("res://Modules/JunkieRehabilitationModule/Scenes/Overwrites/DrugDenEventWhoreScene.gd")
 	GlobalRegistry.registerScene("res://Modules/JunkieRehabilitationModule/Scenes/Overwrites/DrugDenEventWhoreSubScene.gd")
+
+func getPronounChancePercent_they() -> float:
+	return 9.0 # %, set to any desired amount
+
+func getPronounChancePercent_it() -> float:
+	return 1.0 # %, set to any desired amount
+
+func getPronounChancePercent_default() -> float:
+	return max(
+		(
+				100.0
+			- getPronounChancePercent_they()
+			- getPronounChancePercent_it()
+		),
+		0.0
+	)
+
+func pickPronounsGender():
+	return RNG.pickWeightedPairs([
+		[
+			null,
+			getPronounChancePercent_default()
+		],
+		[
+			Gender.Androgynous,
+			getPronounChancePercent_they()
+		],
+		[
+			Gender.Other,
+			getPronounChancePercent_it()
+		],
+	])
--- Modules/JunkieRehabilitationModule/Scenes/Overwrites/DrugDenEncounterTemplateScene.gd
+++ Modules/JunkieRehabilitationModule/Scenes/Overwrites/DrugDenEncounterTemplateScene.gd
@@ -6,6 +6,9 @@ var npcID:String = ""
 var wonState:String = ""
 var expWin:int = 50
 
+var scenario_missingPerson_started:bool = false
+var scenario_missingPerson_foundForSeekerName:String = ""
+
 func _init():
 	sceneID = "DrugDenEncounterTemplateScene"
 
@@ -15,6 +18,8 @@ func generateJunkieNPCID(_isBoss:bool = false) -> String:
 		NpcGen.Temporary: true,
 		NpcGen.IsBoss: _isBoss,
 	})
+	theChar.npcPronounsGender = GlobalRegistry.getModule("JunkieRehabilitationModule").pickPronounsGender()
+
 	var theID:String = theChar.id
 	
 	return theID
@@ -46,6 +51,116 @@ func returnJunkieRehabilitationPillsFromDrugDenStashToPC():
 	for itemToReturn in itemsToReturnToPC:
 		drugDenChar.getInventory().removeItem(itemToReturn)
 		GM.pc.getInventory().addItem(itemToReturn)
+
+func scenario_missingPerson_rollForShouldStart() -> bool:
+	var wasScenarioTriggered:bool = false
+
+	var wasScenarioJustCompleted:bool = (scenario_missingPerson_foundForSeekerName != "")
+
+	if(wasScenarioJustCompleted == true):
+		wasScenarioTriggered = false
+		return wasScenarioTriggered
+
+	var isScenarioAlreadyActive:bool = scenario_missingPerson_isActive()
+
+	if(isScenarioAlreadyActive == true):
+		wasScenarioTriggered = false
+		return wasScenarioTriggered
+
+	var SCENARIO_CHANCE_MILLIONTH_INITIAL_VALUE:int = -10000
+	var SCENARIO_CHANCE_MILLIONTH_INCREMENT:int = 2000
+
+	var chanceAdditiveMillionth:int = GM.main.getFlag("JunkieRehabilitationModule.Scenario_MissingPerson_ChanceMillionth", SCENARIO_CHANCE_MILLIONTH_INITIAL_VALUE)
+	chanceAdditiveMillionth += SCENARIO_CHANCE_MILLIONTH_INCREMENT
+	GM.main.setFlag("JunkieRehabilitationModule.Scenario_MissingPerson_ChanceMillionth", chanceAdditiveMillionth)
+
+	wasScenarioTriggered = RNG.chance(chanceAdditiveMillionth / 10000.0)
+
+	if(wasScenarioTriggered == true):
+		GM.main.setFlag("JunkieRehabilitationModule.Scenario_MissingPerson_ChanceMillionth", SCENARIO_CHANCE_MILLIONTH_INITIAL_VALUE)
+
+	return wasScenarioTriggered
+
+func scenario_missingPerson_isActive() -> bool:
+	return ( GM.main.getFlag("JunkieRehabilitationModule.Scenario_MissingPerson_Name", "") != "" )
+
+func scenario_missingPerson_generateDetails() -> Array:
+	var missingPerson_details:Array = []
+
+	var missingPerson_them:String = "them"
+	var randomPronounsGender = GlobalRegistry.getModule("JunkieRehabilitationModule").pickPronounsGender()
+
+	if(randomPronounsGender == Gender.Androgynous):
+		missingPerson_them = "them"
+	elif(randomPronounsGender == Gender.Other):
+		missingPerson_them = "it"
+	else:
+		missingPerson_them = (
+				"him"
+			if ( GM.main.getEncounterSettings().generateGender() in [NpcGender.Male, NpcGender.Peachboy] )
+			else "her"
+		)
+
+	var defeatedJunkie_name:String = getCharacter(npcID).getName()
+	var missingPerson_name:String = defeatedJunkie_name
+
+	while(missingPerson_name == defeatedJunkie_name):
+		if(missingPerson_them == "her"):
+			missingPerson_name = RNG.randomFemaleName()
+		elif(missingPerson_them == "him"):
+			missingPerson_name = RNG.randomMaleName()
+		else:
+			missingPerson_name = RNG.randomFemaleName() if RNG.chance(50) else RNG.randomMaleName()
+
+	missingPerson_details = [
+		missingPerson_name,
+		missingPerson_them,
+		defeatedJunkie_name
+	]
+
+	GM.main.setFlag("JunkieRehabilitationModule.Scenario_MissingPerson_Name", missingPerson_name)
+	GM.main.setFlag("JunkieRehabilitationModule.Scenario_MissingPerson_PronounThem", missingPerson_them)
+	GM.main.setFlag("JunkieRehabilitationModule.Scenario_MissingPerson_SeekerName", defeatedJunkie_name)
+	GM.main.setFlag("JunkieRehabilitationModule.Scenario_MissingPerson_SearchAttempts", 0)
+
+	return missingPerson_details
+
+func scenario_missingPerson_checkForWasCompleted() -> bool:
+	var wasScenarioCompleted:bool = false
+
+	var missingPerson_searchAttempts:int = GM.main.getFlag("JunkieRehabilitationModule.Scenario_MissingPerson_SearchAttempts", 0)
+	missingPerson_searchAttempts += 1
+	GM.main.setFlag("JunkieRehabilitationModule.Scenario_MissingPerson_SearchAttempts", missingPerson_searchAttempts)
+
+	var defeatedJunkie_character:BaseCharacter = getCharacter(npcID)
+
+	var missingPerson_them:String = GM.main.getFlag("JunkieRehabilitationModule.Scenario_MissingPerson_PronounThem", "")
+	var defeatedJunkie_them:String = defeatedJunkie_character.himHer()
+
+	if(defeatedJunkie_them != missingPerson_them):
+		wasScenarioCompleted = false
+		return wasScenarioCompleted
+
+	var completionChance:float = -20.0 + 15.0 * missingPerson_searchAttempts
+	wasScenarioCompleted = RNG.chance(completionChance)
+
+	if(wasScenarioCompleted == true):
+		var missingPerson_name:String = GM.main.getFlag("JunkieRehabilitationModule.Scenario_MissingPerson_Name", "")
+
+		if(missingPerson_name != ""):
+			defeatedJunkie_character.npcName = missingPerson_name
+
+		var missingPerson_seekerName:String = GM.main.getFlag("JunkieRehabilitationModule.Scenario_MissingPerson_SeekerName", "")
+
+		if(missingPerson_seekerName != ""):
+			scenario_missingPerson_foundForSeekerName = missingPerson_seekerName
+
+		GM.main.setFlag("JunkieRehabilitationModule.Scenario_MissingPerson_Name", "")
+		GM.main.setFlag("JunkieRehabilitationModule.Scenario_MissingPerson_PronounThem", "")
+		GM.main.setFlag("JunkieRehabilitationModule.Scenario_MissingPerson_SeekerName", "")
+		GM.main.setFlag("JunkieRehabilitationModule.Scenario_MissingPerson_SearchAttempts", 0)
+
+	return wasScenarioCompleted
 
 func _react_scene_end(_tag, _result):
 	if(_tag == "encounterFight"):
@@ -97,8 +212,8 @@ func encounter_run():
 	if(state == "gave_pill"):
 		playAnimation(StageScene.SexOral, "start", {pc="pc", npc=npcID})
 
-		saynn("You produce a \"JunkieBeMine\" pill and force them to swallow it.")
-		
+		saynn("You produce a \"JunkieBeMine\" pill and force {npc.him} to swallow "+ ( "the pill" if( getCharacter(npcID).himHer() == "it" ) else "it" ) +".")
+
 		var possible:Array = [
 			"No.. What have you-",
 			"F- Fucker.. What are you-",
@@ -108,7 +223,7 @@ func encounter_run():
 			"I will mess you up- mffmfh..",
 			"I will bite your fucking arm off- ffhfmm..",
 		]
-		
+
 		if(sceneID == "DrugDenEncounterBossScene"):
 			possible.append_array([
 				"Whore. You don't deserve even a fraction of this lab.. What are y-",
@@ -117,7 +232,7 @@ func encounter_run():
 			])
 		else:
 			possible.append_array([
-				"So nice of you to share~.",
+				"So nice of you to share~",
 				"Oooh new product! Gimme-ahff~",
 				"Mm, you like watching me swallow? Freak~.. mmhf-",
 				"No need to be so.. hhf.. forcefhul.. Let me savor it..",
@@ -132,36 +247,124 @@ func encounter_run():
 	if(state == "swallowed_pill"):
 		playAnimation(StageScene.Sleeping, "sleep", {pc=npcID})
 
-		saynn( RNG.pick([
-			"Weakened after the fight, {npc.name} falls onto {npc.his} spine, slipping into slumber.",
-			"There is no stamina left in {npc.name} to resist, and {npc.he} {npc.youVerb('collapse')} on {npc.his} side, beside you.",
-			"No more words leave {npc.nameS} mouth. For a brief moment, {npc.he} {npc.youVerb('stare')} at you with an alert expression, then.. {npc.youVerb('doze')} off.",
-			"Lacking strength to defy you, {npc.name} flops on {npc.his} back, closely following your movements for a few seconds, before inevitably falling asleep.",
-		]) )
+		if(scenario_missingPerson_started == true):
+			saynn( RNG.pick([
+				"With no stamina left to fight you, {npc.name} falls onto {npc.his} side. {npc.His} body lays motionless until {npc.his} arm suddenly snaps at you, tightly grabbing you by the ankle, as {npc.his} lips quietly utter something.",
+				"Noticeably weakened after the fight, {npc.name} lays motionless, staring at- No, *through* you. {npc.His} expression is completely void. Moments after, a quiet murmur echoes past, while {npc.his} lips show no sign of motion."
+			]) )
 
-		saynn("[say=pc]"+RNG.pick([
-			"Let's hope this works..",
-			"That should take care of your addiction..",
-			"Hmph. Maybe you are worth surrendering to..",
-			"I'm a little selfish, huh..",
-			"Sorry, can't afford to lose you to this place.",
-			"I hope we meet again.",
-			"Such a hot thing.",
-			"What a charming thing.",
-			"I'd like to know you a little more.. intimately..",
-			"Once I'm done wandering this miserable place, I'll make sure you're taken good care of.",
-			"We've only just met, and I already feel addicted.. Perhaps I'm not the best person to judge you..",
-			"I really don't want to forget you..",
-			"A few more creatures like you, and the cellblock might just feel like a dream..",
-			"It's tempting to just.. lie beside you, for hours.. But I have to make sure the nearby floors are as safe as they can be.",
-			"What was I doing before this? Right.. Honestly, with distractions shaped like you, I could never complain..",
-			"Your voice is so damn hot... Have you passed out already? No worries, I have a feeling I'll be saying that much more often now..",
-			"You've really caught my attention, you know..",
-			"If.. I'm not your type, I'll find someone just for you.",
-			"I'll have to make it up to you later..",
-		])+"[/say]")
+			var missingPerson_name = GM.main.getFlag("JunkieRehabilitationModule.Scenario_MissingPerson_Name", "MissingInmate")
+			var missingPerson_them = GM.main.getFlag("JunkieRehabilitationModule.Scenario_MissingPerson_PronounThem", "them")
+
+			saynn("[say=npc]"+RNG.pick([
+				( missingPerson_name + "... Find.. " + missingPerson_name + ".." ),
+				( missingPerson_name + "... Save.. " + missingPerson_them + ".." ),
+			])+"[/say]")
+
+			if( RNG.chance(50) ):
+				saynn("Huh, someone they know? You nod, a little stunned, as medicine eventually kicks in, making their paw feel numb.")
+				saynn("A name is not a lot of information to go by, especially in a place where noone cares to introduce themselves. You're hit with a blunt reminder that people make connections even under living conditions such as these.")
+			else:
+				saynn("Don't tell me the sole reason they're in the den is to get someone out.. Fuck.. That was their only chance to blend in with the rest of the junkies.")
+				saynn("You just wanted to be someone's savior, didn't you. Better hope the person they were looking for has enough breaths to draw. All you have is a damn name.")
+		else:
+			saynn( RNG.pick([
+				"Weakened after the fight, {npc.name} falls onto {npc.his} spine, slipping into slumber.",
+				"There is no stamina left in {npc.name} to resist, and {npc.he} {npc.youVerb('collapse')} on {npc.his} side, beside you.",
+				"No more words leave {npc.nameS} mouth. For a brief moment, {npc.he} {npc.youVerb('stare')} at you with an alert expression, then.. {npc.youVerb('doze')} off.",
+				"Lacking strength to defy you, {npc.name} flops on {npc.his} back, closely following your movements for a few seconds, before inevitably falling asleep.",
+			]) )
+
+			var pcPersonality:Personality = GM.pc.getPersonality()
+			var pcFetishHolder:FetishHolder = GM.pc.getFetishHolder()
+
+			var pcPersonalitySubbyScore:float = pcPersonality.personalityScoreMax({ PersonalityStat.Subby: 1.0 })
+			var pcIsSubby:bool = pcPersonalitySubbyScore > 0.4
+			var pcIsDommy:bool = pcPersonalitySubbyScore < -0.4
+
+			var pcInterestInRiggingOthers:float = pcFetishHolder.scoreFetishMax({ Fetish.Rigging: 1.0 })
+			var pcLikesRiggingOthers:bool = pcInterestInRiggingOthers >= 0.5
+
+			var junkieSpecies:Array = getCharacter(npcID).getSpecies()
+
+			var possible:Array = [
+				"Let's hope this works..",
+				"That should take care of your addiction..",
+				"Hmph. Maybe you are worth surrendering to..",
+				"I'm a little selfish, huh..",
+				"Sorry, can't afford to lose you to this place.",
+				"I hope we meet again.",
+				"Such a hot thing.",
+				"What a charming thing.",
+				"I'd like to know you a little more.. intimately..",
+				"Once I'm done wandering this miserable place, I'll make sure you're taken good care of.",
+				"We've only just met, and I already feel addicted.. Perhaps I'm not the best person to judge you..",
+				"I really don't want to forget you..",
+				"A few more creatures like you, and the cellblock might just feel like a dream..",
+				"It's tempting to just.. lie beside you, for hours.. But I have to make sure the nearby floors are as safe as they can be.",
+				"What was I doing before this? Right.. Honestly, with distractions shaped like you, I could never complain..",
+				"Your voice is so damn hot... Have you passed out already? No worries, I have a feeling I'll be saying that much more often now..",
+				"You've really caught my attention, you know..",
+				"If.. I'm not your type, I'll find someone just for you.",
+				"I'll have to make it up to you later..",
+				"A fleeting memory of you is simply not enough..",
+				"This damned place may cloud my judgement, but I've no second thoughts about getting you out of here.",
+				"You're a bad influence on me~",
+				"You must've been through a lot. The cellblock's no Elysium, but I've got plans on changing that.",
+				"The prison staff doesn't give two shits for anyone rotting here. They find it useful. You'll have plenty of utensils, you know.. When the consequences are served.",
+				"I know I'd go through a hundred floors just to find you again. This way I know you'll make it through.",
+				"The cellblock's not inherently safer, but it's one place I have contacts in. They'll ensure nobody gives you any problems that you won't ask for.",
+				"Hey it was you who pounced at me, I don't let go so easily~",
+				"There's so many things I want to tell you..",
+				"Is this ethical? I truthfully don't know. Something something the lesser weevil.",
+				"I cannot save everyone. I'm well aware. Each chance I'm given is paid forward. Use yours well, dear.",
+				"This doesn't make us anything. I hope we get close eventually..",
+				"Would you even remember me?",
+			]
+
+			if(pcIsDommy == true):
+				possible.append_array([
+					"You'd make an excellent plaything, dear. Don't let anyone snatch you before I get a good look holding you by the collar~",
+					"Next time I see you roam the cellblock in full consciousness, I'm pinning you against the nearest wall and making you my own~",
+				])
+			elif(pcIsSubby == true):
+				possible.append_array([
+					"I'm already envisioning all the things I'd let you do to me..",
+					"Guh.. Can't wait for you to play with me..",
+					"Why does everyone here have to be so hot.. Fuck..",
+				])
+
+			if(pcLikesRiggingOthers == true):
+				possible.append_array([
+					"I'm already so tempted to bind you in ropes, hh.. If it didn't complicate your traversal at a time when I need you to safely leave the den, I definitely would~",
+				])
+
+			for junkieSpeciesId in junkieSpecies:
+				if( junkieSpeciesId.ends_with("eon") || junkieSpeciesId.ends_with("vee") ):
+					possible.append_array([
+						"I don't care what the data on your collar says, you're a shiny to me~",
+					])
+
+					break
+
+			saynn("[say=pc]"+RNG.pick(possible)+"[/say]")
 
 		addMessage("{npc.name} can now be found around the cellblock.")
+
+		if(scenario_missingPerson_foundForSeekerName != ""):
+			addMessage(
+					"\n[color=#DDFFCC]"
+				+ RNG.pick([
+					( "You found someone "+ scenario_missingPerson_foundForSeekerName +" asked you to save." ),
+					( "This is, undoubtedly, the person that "+ scenario_missingPerson_foundForSeekerName +" wanted you to find." ),
+				])
+				+ " "
+				+ RNG.pick([
+					( "You just don't know that yet." ),
+					( "You'd immediately feel more at ease, but it's not until you see the two of them in the cellblock that you get to sigh in relief." ),
+				])
+				+ "[/color]"
+			)
 
 		addButton("Leave", "Let them recover at their own pace.", "after_swallowed_pill")
 		
@@ -228,7 +431,18 @@ func encounter_react(_action: String, _args):
 	if(_action == "gave_pill"):
 		GM.pc.getInventory().removeXOfOrDestroy("JunkieRehabilitationPill", 1)
 		var _inmateChar:DynamicCharacter = rehabilitateJunkie()
+
+		if( scenario_missingPerson_isActive() == true ):
+			scenario_missingPerson_checkForWasCompleted()
+
 		setState("gave_pill")
+		return true
+	if(_action == "swallowed_pill"):
+		if( scenario_missingPerson_rollForShouldStart() == true ):
+			scenario_missingPerson_generateDetails()
+			scenario_missingPerson_started = true
+
+		setState("swallowed_pill")
 		return true
 	if(_action == "after_swallowed_pill"):
 		playAnimation(StageScene.Solo, "stand")
@@ -275,6 +489,8 @@ func saveData():
 	var data = .saveData()
 	
 	data["npcID"] = npcID
+	data["scn_mp_started"] = scenario_missingPerson_started
+	data["scn_mp_foundForSeekerName"] = scenario_missingPerson_foundForSeekerName
 	
 	return data
 	
@@ -282,6 +498,8 @@ func loadData(data):
 	.loadData(data)
 	
 	npcID = SAVE.loadVar(data, "npcID", "")
+	scenario_missingPerson_started = SAVE.loadVar(data, "scn_mp_started", false)
+	scenario_missingPerson_foundForSeekerName = SAVE.loadVar(data, "scn_mp_foundForSeekerName", "")
 
 func getSceneCreator():
 	if(state in ["wonFight", "gave_pill", "swallowed_pill"]):
